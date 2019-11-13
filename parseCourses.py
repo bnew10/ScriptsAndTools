2dfa657948d58cc31faf33067a34111c220b6ee3
bnew10 <bradnew10@gmail.com>
2019-11-13 14:05:35 -0800

    updated parseCourses for new json keys

--- parseCourses.py
+++ parseCourses.py
@@ -1,9 +1,9 @@
 import json
 
-with open("/Users/Brad/Desktop/courses.json", "r") as myFile:
+with open("/Users/Brad/Desktop/classes.json", "r") as myFile:
     data = myFile.read()
 
 courses = json.loads(data)
 
 for course in courses:
-    print(course['department'] + str(course['courseNumber']) + " " + course['courseName'])
+    print(course['courseCode'] + " " + course['courseName'])
