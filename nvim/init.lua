0376d6b7362945f33c8b195b249c210bbf6df97b
bnew10 <bradnew10@gmail.com>
2024-10-09 12:24:23 -0700

    Add key mappings for better cursor movement between windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,5 +38,10 @@ local defaults = { noremap = true, silent = true }
 
 map('n', '<esc><esc>', ':noh<cr>', defaults)
 
+map('n', '<C-h>', '<C-w>h', defaults)
+map('n', '<C-j>', '<C-w>j', defaults)
+map('n', '<C-k>', '<C-w>k', defaults)
+map('n', '<C-l>', '<C-w>l', defaults)
+
 -- Options
 vim.cmd('set iskeyword+=-') -- treat dash separated words as a word text object
