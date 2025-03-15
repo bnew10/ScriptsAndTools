290ae8c3187379ee33857b3a6dfc1eaa5c2b47ee
bnew10 <bradnew10@gmail.com>
2025-03-15 01:08:18 -0700

    changes sync to a rm and cp

--- media-server/backup-configs.sh
+++ media-server/backup-configs.sh
@@ -9,5 +9,7 @@ echo 'Copying sabnzbd config, "sabnzbd.ini"'
 aws s3 cp /mnt/Seagate/sabnzbd-config/sabnzbd.ini s3://bnew10-storage/sabnzbd-config/
 
 echo 'Syncing servarr dirs'
-aws s3 sync /mnt/Seagate/sonarr-config s3://bnew10-storage/sonarr-config
-aws s3 sync /mnt/Seagate/radarr-config s3://bnew10-storage/radarr-config
+aws s3 rm s3://bnew10-storage/sonarr-config
+aws s3 cp /mnt/Seagate/sonarr-config s3://bnew10-storage/sonarr-config
+aws s3 rm s3://bnew10-storage/radarr-config
+aws s3 cp /mnt/Seagate/radarr-config s3://bnew10-storage/radarr-config
