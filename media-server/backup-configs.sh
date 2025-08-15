85ade08922276f88867715527d1d02578f62bf87
Bradley Newton <bradnew10@gmail.com>
2025-08-14 23:48:21 -0700

    updates config paths

--- media-server/backup-configs.sh
+++ media-server/backup-configs.sh
@@ -3,13 +3,13 @@
 # first time usage of "aws" cli tool requires user to run "aws configure"
 
 echo 'Copying plex config, "Preferences.xml"'
-aws s3 cp /mnt/Seagate/plex-config/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml s3://bnew10-storage/plex-config/
+aws s3 cp /mnt/Seagate/config/plex-config/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml s3://bnew10-storage/plex-config/
 
 echo 'Copying sabnzbd config, "sabnzbd.ini"'
-aws s3 cp /mnt/Seagate/sabnzbd-config/sabnzbd.ini s3://bnew10-storage/sabnzbd-config/
+aws s3 cp /mnt/Seagate/config/sabnzbd-config/sabnzbd.ini s3://bnew10-storage/sabnzbd-config/
 
 echo 'Syncing servarr dirs'
 aws s3 rm --recursive s3://bnew10-storage/sonarr-config
-aws s3 sync /mnt/Seagate/sonarr-config s3://bnew10-storage/sonarr-config
+aws s3 sync /mnt/Seagate/config/sonarr-config s3://bnew10-storage/sonarr-config
 aws s3 rm --recursive s3://bnew10-storage/radarr-config
-aws s3 sync /mnt/Seagate/radarr-config s3://bnew10-storage/radarr-config
+aws s3 sync /mnt/Seagate/config/radarr-config s3://bnew10-storage/radarr-config
